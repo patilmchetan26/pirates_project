bad76e2f84f895e18987f649599478bb788d48f9
ArtiB Shelar <arti.10723832@ltimindtree.com>
2023-10-28 13:23:01 +0530

    firt commit

---
+++ src/main/java/com/pirates/lab/PiratesOfTheCuesApplication.java
@@ -0,0 +1,13 @@
+package com.pirates.lab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PiratesOfTheCuesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PiratesOfTheCuesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pirates/lab/PiratesOfTheCuesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pirates.lab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PiratesOfTheCuesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
